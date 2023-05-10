1c95cb1bb06b078863fa42417351e0f3d2b6c3fa
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-05-10 16:49:39 +0100

    fixed err

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React , { useState , useEffect} from 'react'
-import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {v4} from 'uuid';
 import './App.css';
 import Header from './Header'
